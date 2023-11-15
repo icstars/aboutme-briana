dafee6bb06bcb363d60e9ad870f30ebfbb13a908
bwildericstar <149719480+bwildericstar@users.noreply.github.com>
2023-11-15 13:34:47 -0600

    x

---
+++ index.js
@@ -0,0 +1,50 @@
+let number= 10
+number = 25
+console.log (number)
+
+//Const
+
+const name = 'Collin'
+
+//name1  = 'Vince'
+
+// Var
+
+//Data Types
+
+//Number/interger
+let x = 7
+let y= 7
+let z = x + y
+
+console.log(z)
+
+//String
+//'', "", ``
+
+const name2 = "Good Morning"
+const name3 = ", How are you?"
+console.log (name2 + name3)
+
+let sentence = 'hi'
+let sentence2 = 'bye'
+let sentence3= sentence +' '+ sentence2
+let sentence4= `Hi, My name is Briana ${sentence2}`
+
+
+let num = '20000'
+let num2 = '20'
+
+console.log (num+num2)
+
+//Booleans
+
+
+//Truthy
+
+//Falsy
+
+console.log(Boolean(undefined))
+
+
+
