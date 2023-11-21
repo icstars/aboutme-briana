4bf5bbace781faeb22daff05d0d8b3c7f93d5fa6
bwildericstar <149719480+bwildericstar@users.noreply.github.com>
2023-11-21 12:41:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,26 @@
-let number= 10
-number = 25
-console.log (number)
+function getUserChoice() {
+   
+     let input = prompt("Choose rock, paper, or scissors");
+    return input.toLowerCase();
+    }
+     function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    let randomIndex = Math.floor(Math.random() * 3);
+     return choices[randomIndex];
+    }
+    
+    function determineWinner(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+     return "It's a tie!";
+        }}
+    
+    if (userChoice === "rock") {
+     if (computerChoice === "scissors")} {
+    return "You win!";
+     } else {
+    return "Computer wins"
+}
 
-//Const
-
-const name = 'Collin'
-
-//name1  = 'Vince'
-
-// Var
-
-//Data Types
-
-//Number/interger
-let x = 7
-let y= 7
-let z = x + y
-
-console.log(z)
-
-//String
-//'', "", ``
-
-const name2 = "Good Morning"
-const name3 = ", How are you?"
-console.log (name2 + name3)
-
-let sentence = 'hi'
-let sentence2 = 'bye'
-let sentence3= sentence +' '+ sentence2
-let sentence4= `Hi, My name is Briana ${sentence2}`
-
-
-let num = '20000'
-let num2 = '20'
-
-console.log (num+num2)
-
-//Booleans
-
-
-//Truthy
-
-//Falsy
-
-console.log(Boolean(undefined))
 
 
 
