7fae90862d700d83c82d64bd5e2d3dc7fbf7741c
bwildericstar <149719480+bwildericstar@users.noreply.github.com>
2023-12-19 22:25:13 -0600

    added javascript
    x

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
-function getUserChoice() {
-   
-     let input = prompt("Choose rock, paper, or scissors");
-    return input.toLowerCase();
-    }
-     function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"];
-    let randomIndex = Math.floor(Math.random() * 3);
-     return choices[randomIndex];
-    }
-    
-    function determineWinner(userChoice, computerChoice) {
-    if (userChoice === computerChoice) {
-     return "It's a tie!";
-        }}
-    
-    if (userChoice === "rock") {
-     if (computerChoice === "scissors")} {
-    return "You win!";
-     } else {
-    return "Computer wins"
+// Function to create a new user object
+function createUser(username, email, password) {
+    return { username, email, password };
 }
 
+// Array to store user objects
+let users = [];
 
+// Function to add a new user
+function addNewUser() {
+    // Prompting for user details
+    let username = prompt("Enter username:");
+    let email = prompt("Enter email:");
+    let password = prompt("Enter password:");
 
+    // Creating a new user object and adding it to the array
+    let newUser = createUser(username, email, password);
+    users.push(newUser);
 
+    // Printing the array to the console
+    console.log(users);
+}
+
+// Function to display a user's details
+function displayUser() {
+    let username = prompt("Enter the username of the user you want to see:");
+    let foundUser = users.find(user => user.username === username);
+
+    if (foundUser) {
+        console.log(foundUser);
+    } else {
+        console.log("User not found.");
+    }
+}
+
+// Main loop
+while (true) {
+    addNewUser();
+
+    if (!confirm("Do you want to make another user?")) {
+        if (confirm("Do you want to see a user?")) {
+            displayUser();
+        }
+        break;
+    }
+}
